c1ac69f9f182861e7100e2281a43e9c8db0a9fe8
soundarya2504 <soundarya8838@gmail.com>
2025-03-05 09:43:21 +0530

    Update AuthContext.js

--- Frontend/recp/src/Component/AuthContext.js
+++ Frontend/recp/src/Component/AuthContext.js
@@ -1,24 +1,23 @@
 import { createContext, useState, useEffect } from "react";
 
-// Create AuthContext
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    // Check local storage for login state
+ 
     const [user, setUser] = useState(() => {
         return JSON.parse(localStorage.getItem("user")) || null;
     });
 
-    // Function to login
+
     const login = (userData) => {
         setUser(userData);
-        localStorage.setItem("user", JSON.stringify(userData)); // Store in local storage
+        localStorage.setItem("user", JSON.stringify(userData));
     };
 
-    // Function to logout
     const logout = () => {
         setUser(null);
-        localStorage.removeItem("user"); // Remove from local storage
+        localStorage.removeItem("user"); 
     };
 
     return (
